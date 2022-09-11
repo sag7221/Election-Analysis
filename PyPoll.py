6ca1d5aee7b6a950172a80e3bf29668e9eb5b861
sag7221 <toshilpa@gmail.com>
2022-09-10 23:21:12 -0700

    written code to write election results to text file

--- PyPoll.py
+++ PyPoll.py
@@ -21,15 +21,30 @@ with open(csv_file_path, "r") as csv_file_reader:
         if row[2] not in total_candidates_list:
             total_candidates_list.append(row[2])
 
+        # Total number of votes received by every candidate
         if row[2] not in candidate_votes_dictionary:
             candidate_votes_dictionary[row[2]] = 1
         else:
             candidate_votes_dictionary[row[2]] += 1
 
-
+# The total number of votes cast in this election
 print("total_number_of_votes: ", total_vote_count)
+# Complete list of candidates who received votes
 print("total_candidates_list: ", total_candidates_list)
 print("candidate_votes_dictionary: ", candidate_votes_dictionary)
+
+file_to_save = os.path.join("analysis", "election_analysis.txt")
+
+election_summary = (
+    f"\n Election Results\n"
+    f"------------------------\n"
+    f"Total Votes: {total_vote_count:,}\n"
+    f"------------------------\n"
+)
+
+print(election_summary)
+with open(file_to_save, "a") as results_file:
+    results_file.write(election_summary)
 
 # Calculate vote percentage for each candidate
 vote_percentage = 0
@@ -37,31 +52,43 @@ winning_candidate = ""
 winning_count = 0
 winning_percentage = 0
 for key in candidate_votes_dictionary.keys():
-    votes = candidate_votes_dictionary[key]
-    vote_percentage = (votes/total_vote_count) * 100
-    print(f"\n{key}: {vote_percentage:.1f}% ({votes:,})\n")
+    with open(file_to_save, "a") as results_file:
+        votes = candidate_votes_dictionary[key]
+        vote_percentage = (votes / total_vote_count) * 100
+        # Percentage of votes won by each candidate
+        print(f"\n{key}: {vote_percentage:.1f}% ({votes:,})\n")
+        candidate_summary = f"\n{key}: {vote_percentage:.1f}% ({votes:,})\n"
+        results_file.write(candidate_summary)
 
-    if(votes > winning_count) and (vote_percentage > winning_percentage):
-        winning_count = votes
-        winning_percentage = vote_percentage
-        winning_candidate = key
+        if(votes > winning_count) and (vote_percentage > winning_percentage):
+            winning_count = votes
+            winning_percentage = vote_percentage
+            winning_candidate = key
 
-# print("---------------------------------------------------------------------------------------------------------------")
-# print(f"Winner is: {winning_candidate} with vote count: {winning_count:,} and vote percentage: {winning_percentage:.1f}%")
-# print("----------------------------------------------------------------------------------------------------------------")
+with open(file_to_save, "a") as results_file:
+    # Winner of the election based on popular vote
+    winning_candidate_summary = (
+        f"-------------------------\n"
+        f"Winner: {winning_candidate}\n"
+        f"Winning Vote Count: {winning_count:,}\n"
+        f"Winning Percentage: {winning_percentage:.1f}%\n"
+        f"-------------------------\n")
+    print(winning_candidate_summary)
+    results_file.write(winning_candidate_summary)
+    results_file.close()
 
-winning_candidate_summary = (
-    f"-------------------------\n"
-    f"Winner: {winning_candidate}\n"
-    f"Winning Vote Count: {winning_count:,}\n"
-    f"Winning Percentage: {winning_percentage:.1f}%\n"
-    f"-------------------------\n")
-print(winning_candidate_summary)
 
 
-# The total number of votes cast in this election
-# Complete list of candidates who received votes
-# Percentage of votes won by each candidate
-# Total number of votes received by every candidate
-# Winner of the election based on popular vote
+
+
+
+
+
+
+
+
+
+
+
+
 
